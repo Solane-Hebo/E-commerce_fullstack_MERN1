023f6d2b8ed2cff4b8edd81ff4861db90b02b0e9
Solane Hebo <solanesani1@gmail.com>
2025-03-19 22:22:25 +0100

    updated message.model

--- Backend/src/models/message.model.js
+++ Backend/src/models/message.model.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose"
 const messageSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email']},
-    message:{type: String, required: true},
-    // user
+    // use
     thread:{type: mongoose.Schema.Types.ObjectId, ref: 'Thread', required: false}
 
 }, {timestamps: true})
