87fbdbeea5be0f11eed4797c6425449a9592b8b6
Solane Hebo <solanesani1@gmail.com>
2025-07-16 15:32:30 +0200

    live

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,6 @@
 import express from "express"
+import cors from 'cors'
+import path from "path"
 import { errorHandler, notFound } from "./middleware/error.middleware.js"
 
 import messageRoutes from "./routes/message.router.js"
@@ -8,13 +10,14 @@ import uploadRouter from "./routes/upload.router.js"
 import userRoutes from "./routes/user.route.js"
 import orderRoutes from "./routes/order.route.js"
 import cartRoute from './routes/cart.router.js'
-import cors from 'cors'
 
 
 
 const app = express()
 
 const whitelist = ['http://localhost:5174', ' http://localhost:7676', '*']
+
+const __dirname = path.resolve()
 
 app.use(cors())
 app.use(express.json())
@@ -38,6 +41,14 @@ app.use('/api/cart', cartRoute)
 
 // Serve static images from public/images
 app.use('/images', express.static('public/images'));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')))
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'))
+    })
+}
 
 
 // Error handling middleware
--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = 'http://localhost:7676'
+const BASE_URL = import.meta.env.MODE === 'developmet' ? 'http://localhost:7676' : '/'
 
 export default axios.create({
     baseURL: BASE_URL
